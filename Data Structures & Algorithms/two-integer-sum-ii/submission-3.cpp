6d71ad8edf8359cb1c1d19253b5227f3da9d2676
Nikola Trifunovic <nik.trifun@gmail.com>
2026-08-23 17:31:17 +0200

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> result;
+        int i = 0;
+        int j = numbers.size() - 1;
+        int sum = numbers[i] + numbers[j];
+        while (sum != target) {
+            sum < target ? i++ : j--;
+            sum = numbers[i] + numbers[j];
+        }
+
+        result.push_back(i + 1);
+        result.push_back(j + 1);
+
+        return result;
+    }
+};
