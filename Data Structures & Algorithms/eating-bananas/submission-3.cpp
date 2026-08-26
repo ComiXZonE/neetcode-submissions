efae0bccfb1c9936730e2bca620361440f4cec00
Nikola Trifunovic <nik.trifun@gmail.com>
2026-08-26 14:51:13 +0200

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1, right = piles[0];
+
+        for (int pile : piles) {
+            right = max(right, pile);
+        }
+
+        int result = right;
+//        int prev_time = 0;
+        while (left <= right) {
+            int k = (left + right) / 2;
+            int time = hoursNeeded(piles, k);
+
+            if (time > h) {
+                left = k + 1;
+            }
+            else {
+//                if (time >= prev_time) {
+//                    prev_time = time;
+                    result = k;
+//                }
+                right = k - 1;
+            }
+        }
+
+        return result;
+    }
+
+    int hoursNeeded(vector<int>& piles, int k) {
+        int result = 0;
+        for (int pile : piles) {
+            result += (pile / k) + (pile % k != 0);
+        }
+
+        return result;
+    }
+};
