287d7b098756b905dae5c5644e467b57c5c06c38
Nikola Trifunovic <nik.trifun@gmail.com>
2026-09-17 01:16:31 +0200

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        stack<tuple<int, int, int>> val_idx;  // val, idx, start
+        vector<int> comb;
+
+        sort(candidates.begin(), candidates.end());
+
+        val_idx.push({target, 0, 0});
+        while (!val_idx.empty()) {
+            auto& [val, idx, start] = val_idx.top();
+
+            // advance past duplicate values at this level, without leaving this iteration
+            while (idx < candidates.size() && idx > start &&
+                   candidates[idx] == candidates[idx - 1]) {
+                idx++;
+            }
+
+            if (idx == candidates.size() || val < candidates[idx]) {
+                val_idx.pop();
+                comb.pop_back();
+                continue;
+            }
+
+            comb.push_back(candidates[idx]);
+
+            if (val == candidates[idx]) {
+                result.push_back(comb);
+                comb.pop_back();
+            } else {
+                val_idx.push({val - candidates[idx], idx + 1, idx + 1});
+            }
+
+            idx++;
+        }
+
+        return result;
+    }
+};
